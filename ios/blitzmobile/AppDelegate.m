36c48dd9770a352d4fa10ee36a8d79c24385cbf0
Sonny Lazuardi <sonnylazuardi@gmail.com>
2016-09-28 16:11:37 +0700

    debug config

--- ios/blitzmobile/AppDelegate.m
+++ ios/blitzmobile/AppDelegate.m
@@ -15,8 +15,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-  //jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
-  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  //jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
   
   
   // **********************************************
